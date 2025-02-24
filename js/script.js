472f19430e45c6b6aa227bc27a6cc7b144d10280
MHTuhin <hasibulislamtuhin@gmail.com>
2025-02-24 09:23:58 +0600

    Full Completed

--- js/script.js
+++ js/script.js
@@ -312,3 +312,13 @@ document.addEventListener("DOMContentLoaded", function () {
 	window.addEventListener("resize", setCollectionListWidth);
 });
 
+document.getElementById("cards").onmousemove = (e) => {
+	for (const card of document.getElementsByClassName("resources_item")) {
+		const rect = card.getBoundingClientRect(),
+			x = e.clientX - rect.left,
+			y = e.clientY - rect.top;
+
+		card.style.setProperty("--mouse-x", `${x}px`);
+		card.style.setProperty("--mouse-y", `${y}px`);
+	}
+};
